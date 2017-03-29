ffc8659b80856cec259b59448529c5e882176044
Cristian Caucott <criss.acv@gmail.com>
2017-03-29 16:30:29 -0300

    Adaptar avances de los archivos de front, con los de la aplicación en Rails.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'main#index'
+
+  get '/espacio', to: 'main#espacio', as: 'espacio'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
