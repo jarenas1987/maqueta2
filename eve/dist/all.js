5f09007d918a06ef353ba9ddea9a25a29ac54ef7
Eveperalta <eperalta.cm@gmail.com>
2017-03-31 12:39:13 -0300

    nuevo diseño, corregi los conflictos

--- eve/dist/all.js
+++ eve/dist/all.js
@@ -1,29 +1,23 @@
 //dropdown menu
 $( document ).ready(function(){
-    $(".dropdown-button").dropdown();
+  $(".dropdown-button").dropdown();
 });
 
- // Initialize collapsible (uncomment the line below if you use the dropdown variation)
- $('.collapsible').collapsible();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+$('.collapsible').collapsible();
 
-$(document).ready(function() {//initialize select
-   $('select').material_select();
- });
 
- $('.carousel').carousel();
- $('.modal').modal();
- $(document).ready(function(){
-       $('.carousel').carousel({
-             dist:0,
-             shift:0,
-             padding:150,
 
-       });
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
 
-     });
-     $(document).ready(function(){
-       $('.materialboxed').materialbox();
-     });
+$("#scale-demo").on("click", function(e){
+  $("#add").removeClass("hide");
+})
 
 /*!
  * Materialize v0.98.1 (http://materializecss.com)
