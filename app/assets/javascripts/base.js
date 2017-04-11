489012a9bb4bb9c39311561f517935c866ebe544
Eveperalta <eperalta.cm@gmail.com>
2017-04-11 10:28:23 -0300

    removed leanmodal

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -267,7 +267,7 @@ $(document).ready(function(){
 });
 $(document).ready(function() {
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-  $('.modal-trigger').leanModal({
+  $('.modal-trigger').modal({
     dismissible:false
   });
 });
