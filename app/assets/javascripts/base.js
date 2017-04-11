4c473ee001f67648970c13582c274a26728f1e35
Eveperalta <eperalta.cm@gmail.com>
2017-04-11 10:02:33 -0300

    corregi el js

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -265,11 +265,3 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('.tooltipped').tooltip({delay: 50});
 });
-$(document).ready(function() {
-  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-  $('.modal-trigger').leanModal();
-});
-
-$("#buttonModal1").click(function() {
-  $('#modal2').closeModal();
-});
