585a0308dbb237c97311cec7d5689deb8667973d
Cristian Caucott <criss.acv@gmail.com>
2017-04-11 09:44:54 -0300

    prueba

--- app/mailers/pdf_mailer.rb
+++ app/mailers/pdf_mailer.rb
@@ -2,6 +2,7 @@ class PdfMailer < ApplicationMailer
 
 	def pdf_email(email, pdf_path)
 		attachments['experiencia.pdf'] = File.read(pdf_path)
+		puts "ENV: #{ENV['username']}"
   	mail_gun_stat = mail(from: ENV['username'], to: email, subject: 'Prueba de email app maqueta')
 	end
 end
